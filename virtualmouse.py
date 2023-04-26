a18cf2f9c0aa26eec3243c8951ef55116fce0832
Mitesh <miteshkhandait1234@gmail.com>
2023-04-26 16:16:06 +0530

    right click implemented
    26/4/23

--- virtualmouse.py
+++ virtualmouse.py
@@ -3,10 +3,11 @@ import numpy as np
 import handtrackingmodule as htm
 import time
 import autopy
+import pyautogui
 
 
 wCam, hCam = 640, 480
-frameR = 100 #Frame reduction
+frameR = 100            #Frame reduction
 smoothening = 8.7
 #################################
 
@@ -20,6 +21,7 @@ cap.set(3, wCam)
 cap.set(4, hCam)
 detector = htm.handDetector(maxHands=1)
 wScr, hScr = autopy.screen.size()
+
 # print(wScr, hScr)
 
 
@@ -30,29 +32,51 @@ while True:
 
     #2. get the tip of the index and middle finger
 
-    if len(lmList)!=0:
+    if len(lmList) != 0:
         x1, y1 = lmList[8][1:]
         x2, y2 = lmList[12][1:]
 
         # print(x1, y1, x2, y2)
 
         #3. Check which fingers are up
+
         fingers = detector.fingersUp()
         # print(fingers)
         cv2.rectangle(img, (frameR, frameR), (wCam - frameR, hCam - frameR), (255, 0, 255), 2)
+
         #4. If only the Index finger is Up
+
         if fingers[1] == 1 and fingers[2] == 0:
 
             #5. Convert the Cordinates
+
             x3 = np.interp(x1, (frameR, wCam-frameR), (0, wScr))
             y3 = np.interp(y1, (frameR, hCam-frameR), (0, hScr))
 
             #6. Move mouse
+
             clocX = plocX + (x3 - plocX) / smoothening
             clocY = plocY + (y3 - plocY) / smoothening
             autopy.mouse.move(wScr-clocX, clocY)
             cv2.circle(img, (x1, y1), 11, (0, 255, 0), cv2.FILLED)
             plocX , plocY = clocX, clocY
+
+        #the right click function
+
+        if fingers[1] == 0 and fingers[2] == 1:
+            # 5. Convert the Cordinates
+            x4 = np.interp(x1, (frameR, wCam - frameR), (0, wScr))
+            y4 = np.interp(y1, (frameR, hCam - frameR), (0, hScr))
+
+            # 6. Move mouse
+
+            clocX = plocX + (x4 - plocX) / smoothening
+            clocY = plocY + (y4 - plocY) / smoothening
+            pyautogui.rightClick(wScr - clocX, clocY)
+            cv2.circle(img, (x1, y1), 11, (0, 255, 0), cv2.FILLED)
+            plocX, plocY = clocX, clocY
+
+
         #7. Both Index and middle finger are up
 
         if fingers[1] == 1 and fingers[2] == 1:
@@ -62,9 +86,11 @@ while True:
             print(length)
 
             # 9. left click function
+
             if length <36:
                 cv2.circle(img, (lineInfo[4], lineInfo[5]), 11, (0, 255, 0), cv2.FILLED)
                 autopy.mouse.click()
+
 
     cTime = time.time()
     fps = 1/(cTime-pTime)
